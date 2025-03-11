7f96845d46df2017ad79c5fb7fca8c29fb8a7b6e
MayanRivlin <mayanriv@gmail.com>
2025-03-11 19:05:53 +0200

    added visualization evaluate

--- code/AutoEncoderDecoder.py
+++ code/AutoEncoderDecoder.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class EncoderCIFAR(torch.nn.Module):
+class EncoderCIFAR(nn.Module):
     def __init__(self, latent_dim):
         super().__init__()
         self.encoder = torch.nn.Sequential(
